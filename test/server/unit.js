f146d66bb331670349f64f9d1b1700cf95d6c5c6
Jimmy Zhen <jimmy@azuqua.com>
2017-11-08 11:47:40 -0800

    Changed string.includes() call to string.indexOf() to support older node versions

--- test/server/unit.js
+++ test/server/unit.js
@@ -51,7 +51,7 @@ module.exports = function(TokenSocket, mocks){
 				assert.doesNotThrow(function(){
 					var socket = new TokenSocket({ host: 'http://foo.com'	});
 					var req = socket._rest._requests.shift();
-					assert.isNotOk(req.options.host.includes('http'), 'parsed host does not include protocol');
+					assert.isOk(req.options.host.indexOf('http') === -1, 'parsed host does not include protocol');
 				}, "Constructor does not create an invalid url when passed a host argument that includes protocol");
 			});
 			
